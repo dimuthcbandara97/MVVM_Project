d6f631b86d6f9db2400de5f3f8de3b7e27b33c77
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-01 09:50:41 +0530

    Initial Commit

---
+++ MVVM_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Project
+//
+//  Created by Dimuth Bandara on 2023-03-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
